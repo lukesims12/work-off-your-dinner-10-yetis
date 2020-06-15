1b4b975b1f4357add762ca83c347f95060042936
luke.sims <lukesims12@gmail.com>
2020-06-15 09:58:38 +0100

    Slide animation work.

--- app.js
+++ app.js
@@ -12,11 +12,11 @@ $(document).ready(function() {
     $.getJSON("./assets/json/drinks.json", function(data) {
         $.each(data, function( i ) {
             // name of each drink
-            //console.log(data[0].options[i].name);
+            // console.log(data[0].options[i].name);
             // individual amount of boxes needed
             const numOptions = parseInt(data[0].options.length) - 1;
             const numOptionRows = Math.ceil((parseInt(data[0].options.length) / 3 ) );
-            $("#option-container").append(displayLayout(numOptionRows, numOptions));
+            // $("#option-container").append(displayLayout(numOptionRows, numOptions));
             // console.log(numOptionRows);
             // console.log(data[0].options.length);
             
@@ -29,11 +29,11 @@ $(document).ready(function() {
     $.getJSON(`./assets/json/${selectedOption}.json`, function(data) {
         $.each(data, function( i ) {
             // name of each drink
-            //console.log(data[0].options[i].name);
+            // console.log(data[0].options[i].name);
             // individual amount of boxes needed
             const numOptions = parseInt(data[0].options.length) - 1;
             const numOptionRows = Math.ceil((parseInt(data[0].options.length) / 3 ) );
-            //$("#option-container").append(displayLayout(numOptionRows, numOptions));
+            // $("#option-container").append(displayLayout(numOptionRows, numOptions));
             // console.log(numOptionRows);
             console.log(data[0].options.length);
             console.log('options');
@@ -54,21 +54,29 @@ $(document).ready(function() {
             html += `<div class="option-container-${i + 1}">
                 <div></div>`;
                 if (numberOfOptions >= counter) {
-                    html += `<div class="option option-${counter += 1}">`;                
+                    html += `<div class="option option-${counter += 1}"></div>`;                
                 }
-            html += `</div>`;
                 if (numberOfOptions >= counter) {
-                    html += `<div class="option option-${counter += 1}">`;
+                    html += `<div class="option option-${counter += 1}"></div>`;
                 }
-            html += `</div>`;
                 if (numberOfOptions >= counter) {
-                    html += `<div class="option option-${counter += 1}">`;
+                    html += `<div class="option option-${counter += 1}"></div>`;
                 }
-            html += `</div>
-                <div></div>
+            html += `<div></div>
             </div>`;
         }
         return html;
     }
+
+    // ### SLIDE ANIMATION FOR OPTIONS ### //
+    $('.option').click(function(e) {
+        //$(this).addClass('option-slide');
+        console.log($('.option').position().right);
+        
+        if ($('.option').position().right == '75px') {
+            $(this).animate({ right: '0' }); 
+        }
+        $(this).animate({ right: '75px' }); 
+    });
 
 });
